9c5fef5fdfadf43555ca23963d7cf37e45ac1e09
Mike Creighton <mike@mikecreighton.com>
2024-10-02 14:54:46 -0700

    Adding OpenAI client.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from ratelimiter import RateLimitMiddleware
+from openai import AsyncOpenAI
 
 load_dotenv(override=True)
 
@@ -14,6 +15,13 @@ SERVER_ENV = os.getenv("SERVER_ENV", "production")
 FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN", f"http://localhost:{DEFAULT_PORT}")
 TEMPERATURE = 0.8
 MAX_TOKENS = 4096
+
+if LLM_PROVIDER == "openai":
+    API_BASE_URL = "https://api.openai.com/v1"
+    DEFAULT_LLM_MODEL = "gpt-4o"
+elif LLM_PROVIDER == "openrouter":
+    API_BASE_URL = "https://openrouter.ai/api/v1"
+    DEFAULT_LLM_MODEL = "anthropic/claude-3.5-sonnet"
 
 app = FastAPI()
 # Add the rate limiting middleware
@@ -40,6 +48,8 @@ else:
         allow_methods=["POST", "OPTIONS"],
         allow_headers=["*"],
     )
+
+client = AsyncOpenAI(api_key=LLM_API_KEY, base_url=API_BASE_URL)
 
 
 @app.get("/")
