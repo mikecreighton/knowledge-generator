845617f37af71db2dc17a8efaf0377bba26ad04f
Mike Creighton <mike@mikecreighton.com>
2024-10-03 10:32:38 -0700

    XML validation with retries. Added some additional comments for readers.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.responses import FileResponse
 from ratelimiter import RateLimitMiddleware
 from openai import AsyncOpenAI
 from pydantic import BaseModel
 from xml.etree import ElementTree as ET
 import asyncio
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 load_dotenv(override=True)
 
@@ -18,7 +19,7 @@ LLM_MODEL = os.getenv("LLM_MODEL")
 SERVER_ENV = os.getenv("SERVER_ENV", "production")
 FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN", f"http://localhost:{DEFAULT_PORT}")
 TEMPERATURE = 0.8
-MAX_TOKENS_PER_ARTICLE = 4096
+MAX_TOKENS_PER_SECTION = 4096
 
 if LLM_PROVIDER == "openai":
     API_BASE_URL = "https://api.openai.com/v1"
@@ -30,6 +31,7 @@ elif LLM_PROVIDER == "openrouter":
 app = FastAPI()
 
 if SERVER_ENV == "production":
+    # Rate limiting middleware if this ever gets pushed to production.
     app.add_middleware(
         RateLimitMiddleware,
         limit=5,
@@ -46,6 +48,7 @@ if os.getenv("SERVER_ENV") == "development":
         allow_headers=["*"],
     )
 else:
+    # Only allow requests from the frontend origin in production.
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[os.getenv("FRONTEND_ORIGIN")],
@@ -56,14 +59,62 @@ else:
 
 client = AsyncOpenAI(api_key=LLM_API_KEY, base_url=API_BASE_URL)
 
-
 # Define the model for the incoming topic request
 class Topic(BaseModel):
     topic: str
 
 
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
+async def get_valid_xml_response(client, system_prompt, user_prompt):
+    """
+
+    Get a valid XML response from the LLM.
+    Retry up to 3 times if the response is not valid.
+
+    """
+    response = await client.chat.completions.create(
+        model=LLM_MODEL,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+        temperature=TEMPERATURE,
+        max_tokens=MAX_TOKENS_PER_SECTION,
+    )
+
+    outliner_response = response.choices[0].message.content
+
+    print("\nOutliner Response: -------------------------------")
+    print(outliner_response)
+
+    # Extract XML content between <knowledge> tags
+    start_tag = "<knowledge>"
+    end_tag = "</knowledge>"
+    start_index = outliner_response.find(start_tag)
+    end_index = outliner_response.rfind(end_tag)
+
+    if start_index != -1 and end_index != -1:
+        xml_content = outliner_response[start_index : end_index + len(end_tag)]
+    else:
+        print("XML tags not found, retrying...")
+        raise ValueError("XML tags not found in the response")
+
+    # Attempt to parse XML
+    try:
+        root = ET.fromstring(xml_content)
+        return root, xml_content
+    except ET.ParseError:
+        print("XML parsing failed, retrying...")
+        raise  # This will trigger a retry
+
+
 @app.post("/api/generate")
 async def generate_knowledge(topic: Topic):
+    """
+
+    Generates the long-form article about a topic.
+
+    """
     from prompts import (
         construct_outliner_system_prompt,
         construct_outliner_user_prompt,
@@ -71,91 +122,88 @@ async def generate_knowledge(topic: Topic):
         construct_knowledge_generator_user_prompt,
     )
 
+    print("\n--------------------------------")
+    print("Beginning knowledge generation...")
+
     outliner_system_prompt = construct_outliner_system_prompt()
     outliner_user_prompt = construct_outliner_user_prompt(topic.topic)
 
     try:
-        response = await client.chat.completions.create(
-            model=LLM_MODEL,
-            messages=[
-                {"role": "system", "content": outliner_system_prompt},
-                {"role": "user", "content": outliner_user_prompt},
-            ],
-            temperature=TEMPERATURE,
+        root, outliner_response = await get_valid_xml_response(
+            client, outliner_system_prompt, outliner_user_prompt
+        )
+    except Exception as e:
+        return {
+            "error": f"Failed to get valid XML response after 3 attempts: {str(e)}",
+            "content": "",
+        }
+
+    # Extract topic
+    topic = root.find("topic").text
+
+    # Extract sections and create prompts
+    section_user_prompts = []
+    for section in root.find("sections").findall("section"):
+        title = section.find("title").text
+        section_user_prompts.append(
+            construct_knowledge_generator_user_prompt(title, outliner_response)
         )
 
-        outliner_response = response.choices[0].message.content
+    # Now we have a list of prompts in section_user_prompts
+    # We'll use these to generate content for each section
+    knowledge_generator_system_prompt = construct_knowledge_generator_system_prompt()
 
-        print("\nOutliner Response: -------------------------------", outliner_response)
-
-        # Parse the XML string
-        root = ET.fromstring(outliner_response)
-
-        # Extract topic
-        topic = root.find("topic").text
-
-        # Extract sections and create prompts
-        section_user_prompts = []
-        for section in root.find("sections").findall("section"):
-            title = section.find("title").text
-            section_user_prompts.append(
-                construct_knowledge_generator_user_prompt(title, outliner_response)
+    # Function for generating content for a section...
+    # allows us to generate content in parallel.
+    async def generate_section_content(index, prompt):
+        try:
+            response = await client.chat.completions.create(
+                model=LLM_MODEL,
+                messages=[
+                    {"role": "system", "content": knowledge_generator_system_prompt},
+                    {"role": "user", "content": prompt},
+                ],
+                max_tokens=MAX_TOKENS_PER_SECTION,
+                temperature=TEMPERATURE,
             )
+            return index, response.choices[0].message.content
+        except Exception as e:
+            return index, f"Error: {str(e)}"
 
-        # Now we have a list of prompts in section_user_prompts
-        # We'll use these to generate content for each section
-        knowledge_generator_system_prompt = construct_knowledge_generator_system_prompt()
+    tasks = [
+        generate_section_content(i, prompt) for i, prompt in enumerate(section_user_prompts)
+    ]
 
-        async def generate_section_content(index, prompt):
-            try:
-                response = await client.chat.completions.create(
-                    model=LLM_MODEL,
-                    messages=[
-                        {"role": "system", "content": knowledge_generator_system_prompt},
-                        {"role": "user", "content": prompt},
-                    ],
-                    max_tokens=MAX_TOKENS_PER_ARTICLE,
-                    temperature=TEMPERATURE,
-                )
-                return index, response.choices[0].message.content
-            except Exception as e:
-                return index, f"Error: {str(e)}"
+    section_contents = await asyncio.gather(*tasks)
 
-        tasks = [
-            generate_section_content(i, prompt)
-            for i, prompt in enumerate(section_user_prompts)
-        ]
+    # Check if any sections had errors
+    errors = [content for _, content in section_contents if content.startswith("Error:")]
+    if errors:
+        error_message = "; ".join(errors)
+        return {
+            "error": f"Error generating content: {error_message}",
+            "content": "",
+        }
 
-        section_contents = await asyncio.gather(*tasks)
+    # Sort the results by index to maintain original order
+    sorted_section_contents = sorted(section_contents, key=lambda x: x[0])
 
-        # Check if any sections had errors
-        errors = [content for _, content in section_contents if content.startswith("Error:")]
-        if errors:
-            error_message = "; ".join(errors)
-            return {
-                "error": f"Error generating content: {error_message}",
-                "content": "",
-            }
+    # Extract just the content, discarding the index
+    final_content = [content for _, content in sorted_section_contents]
 
-        # Sort the results by index to maintain original order
-        sorted_section_contents = sorted(section_contents, key=lambda x: x[0])
-
-        # Extract just the content, discarding the index
-        final_content = [content for _, content in sorted_section_contents]
-
-        # Combine all section contents into a single string
-        combined_content = "\n\n".join(final_content)
-
-    except ET.ParseError as e:
-        return {"error": f"XML parsing error: {str(e)}", "content": ""}
-    except Exception as e:
-        return {"error": str(e), "content": ""}
+    # Combine all section contents into a single string
+    combined_content = "\n\n".join(final_content)
 
     return {"content": combined_content, "outline": outliner_response}
 
 
 @app.get("/")
 async def read_index():
+    """
+
+    Serves the frontend.
+
+    """
     return FileResponse("index.html")
 
 
--- prompts.py
+++ prompts.py
@@ -12,13 +12,13 @@ OUTLINE_TEMPLATE = f"""
 """
 
 OUTLINER_SYSTEM_PROMPT = """
-You are an expert at creating outlines for a given topic. Later, you use these outlines to generate knowledge articles to help learners improve their knowledge about a given topic.
+You are an expert at creating outlines for long-form articles about a given topic. Later, you use these outlines to generate long-form articles to help learners improve their knowledge about a given topic.
 
 # Your Inputs
 The topic of the knowledge article, along with any specific details that the learner desires to know about the topic.
 
 # Your Output
-An outline that covers all of the details (at a high level) and is structured as a list of sections.
+An outline for the knowledge article that covers all of the details (at a high level) and is structured as a list of sections.
 
 # Output Format
 - You will use XML format for the outline.
@@ -31,9 +31,9 @@ An outline that covers all of the details (at a high level) and is structured as
 
 {{outline_template}}
 
-- Your outline should contain between 5 and 8 sections. It's good to go into sufficient enough detail that each section could stand on its own as a separate knowledge article.
+- Your outline should contain between 5 and 8 sections. It's good to go into sufficient enough detail that each section could stand on its own as a separate knowledge article, even though it will be part of a single larger article.
 - ONLY output the XML content of your outline. Do not include any preamble or other text. I'll be parsing this with an XML parser, so if you include any text outside of the XML, the parser will throw an error.
-- ONLY XML!!!
+- Please, ONLY XML!!!
 """
 
 
@@ -43,7 +43,7 @@ def construct_outliner_system_prompt() -> str:
     )
     outliner_system_template = outliner_system_template.replace(
         "{{section_content}}",
-        "<!-- The recommended content for the section, which should just be plain English describing the types of things this section should cover. Try to be specific here, so that you remember later all the things you want to cover. -->",
+        "<!-- The recommended content for the section, which should just be plain English describing the types of things this section should cover. Try to be specific here, so that you remember later all the things you want to cover. You should have at least a couple sentences here, but don't go too long. -->",
     )
     outliner_system_template = outliner_system_template.replace(
         "{{additional_sections_note}}",
@@ -61,7 +61,7 @@ Here is the topic I'm interested in learning about, along with some specific det
 
 
 KNOWLEDGE_GENERATOR_SYSTEM_PROMPT = """
-You are an expert at creating knowledge articles for a given topic. You will be given an outline of a topic with a list of sections that should be covered. Each section represents a high-level overview of the content that should be in the article. You will also be given the name of the title of the section that you are creating content for.
+You are an expert at creating long-form knowledge articles for a given topic. You will be given an outline of a topic with a list of sections that should be covered in the long-form article. Each section represents a high-level overview of the content that should be in the article. You will also be given the name of the title of the section that you are creating content for.
 
 # Your Inputs
 - The outline of the knowledge article (in XML format), which will contain a list of sections.
@@ -78,24 +78,27 @@ Your Section Title:
 <title><!-- The title of the section that you are creating content for --></title>
 
 # Your Output
-The entire article for your section, taking into account what will already be covered in the other sections. Your goal is for the article to be thorough, helpful, and approachable.
+All of the written content for your section, taking into account what will already be covered in the other sections. Your goal is for your content to be thorough, helpful, and approachable. It should also make sense in the context of the larger long-form article that it will be a part of.
 
 # Output Format
-- You will use markdown format for the article.
-- You will include the title of your section as a header at the top of the article.
+- You will use markdown format for your writing.
+- You will include the title of your section as a header at the top of your output.
 - The title's header will be formatted as a level 2 header (## Title Case).
-- Do not include any links in the article.
-- Do not include any images in the article.
-- ONLY output the content of your article. Do not include any preamble or other text. I'll be parsing this with a Markdown parser, so if you include any text outside of the markdown, the parser will throw an error.
+- Do not include any links in your writing.
+- Do not include any images in your writing.
+- ONLY output the content of your section. Do not include any preamble or other text. I'll be parsing this with a Markdown parser, so if you include any text outside of the markdown, the parser will throw an error.
+
+# Important Notes
+- Becuase your section is part of a larger article, avoid including a "Conclusion" to your section UNLESS you're writing the final section of the article.
 """
 
 
 def construct_knowledge_generator_system_prompt() -> str:
     knowledge_generator_system_template = OUTLINE_TEMPLATE.replace(
-        "{{topic_input}}", "<!-- The topic that the articles will cover -->"
+        "{{topic_input}}", "<!-- The topic that the long-form article will cover -->"
     )
     knowledge_generator_system_template = knowledge_generator_system_template.replace(
-        "{{section_title}}", "<!-- The title of a section -->"
+        "{{section_title}}", "<!-- The title of a section of the article -->"
     )
     knowledge_generator_system_template = knowledge_generator_system_template.replace(
         "{{section_content}}", "!-- The recommended content for the section. -->"
